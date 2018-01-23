f4a858997942b70e3a8e54002f84e3911f252234
noxgle <hkon@noxgle>
2018-01-23 20:21:19 +0100

    corrections

--- createVirtEnv.bash
+++ createVirtEnv.bash
@@ -3,7 +3,16 @@
 # Script start in project folder.
 DIR=`pwd`
 
-#pigar -P $DIR
+if ! [ -x "$(command -v pip)" ]; then
+  echo 'Error: pip is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v virtualenv)" ]; then
+  echo 'Error: virtualenv is not installed.' >&2
+  exit 1
+fi
+
 virtualenv --no-site-packages venv
 
 source $DIR/venv/bin/activate
